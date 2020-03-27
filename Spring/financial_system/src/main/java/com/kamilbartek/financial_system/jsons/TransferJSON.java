3afdb801021cf3c0ce983967bcd80aa7e3ad97a6
bartekmaciag <Maciag312>
2020-03-27 19:07:45 +0100

    using merge

---
+++ Spring/financial_system/src/main/java/com/kamilbartek/financial_system/jsons/TransferJSON.java
@@ -0,0 +1,8 @@
+package com.kamilbartek.financial_system.jsons;
+
+public class TransferJSON {
+    public Long sender_account_id;
+    public Long reciever_account_id;
+    public double amount;
+    public String currency;
+}
